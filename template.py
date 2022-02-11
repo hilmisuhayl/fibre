3ad6b083e4d21ed6c4f40df1e987dd84fee254c1
Versal <87914958+suuuub@users.noreply.github.com>
2022-02-11 14:15:58 +0300

    Update template.py

--- template.py
+++ template.py
@@ -1,5 +1,27 @@
 from fibre import fibre
 
+"""
+The first example or template on how to use Fibre:
+Iteration over a file.
+"""
+
 for line in fibre.s("fibre.py", save=True, suppress=True):
     if len(line) > 25:
         print(line[0:40] + "..." + line[-10:])
+        
+"""
+The second example or template on how to use Fibre:
+Printing the strings in a file, without iteration.
+"""
+        
+print(fibre.s("fibre.py", save=True, suppress=True))
+
+"""
+The third example or template on how to use Fibre:
+Appedning strings to a list.
+"""
+
+strings = []
+
+for string in fibre.s("fibre.py", save=True, suppress=True):
+    strings.append(string)
