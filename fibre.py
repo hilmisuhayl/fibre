62cf030be5c81e2432d00d0c4c5664ac3ede0b6d
webieu <subforbusiness@gmail.com>
2022-06-01 14:11:28 +0300

    Update fibre.py

--- fibre.py
+++ fibre.py
@@ -4,12 +4,22 @@ class fibre(object):
     def __init__(self, file) -> None:
         self.file = file
         
-    def save(self, errors: bool, save: bool):
+    def save(self, errors: bool, save: bool, filename: str) -> None:
+        """
+        errors:
+            Error restrictions is always a bool. Set as false to ignore errors.
+        save:
+            Saving the output is always a bool. Set as false to ignore saving.
+        filename:
+            The filename is always a string, set as None if you want the default name. Only works if save is true.
+        """
+        
         if errors == True: errors = "ignore"
+        if filename == None: filename = "default.log"
         with open(self.file, "r", errors=errors) as f:
             content = f.read()
             
         s = re.findall("[\x1f-\x7e]{4,}", content)
-        if save: open(".log", "w").write("\n".join(s))
-        else: pass            
+        if save:  open("{}".format(filename), "w").write("\n".join(s))
+        else: pass
         return s
